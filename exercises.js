7481015f1196aa8d9d26bb827148cec2f3c994c5
Justin Lau <jlau7248@gmail.com>
2017-07-10 12:09:04 -1000

    added moneytrain function.

--- exercises.js
+++ exercises.js
@@ -286,7 +286,16 @@ console.log(graduation(122, true));
  * Console.log your result.
 */ 
 
-
+function moneyTrain(speed){
+	if(speed <50){
+		return "You are riding Honolulu's Rail.";
+	}else if(speed <100){
+		return "You are riding an Amtrak";
+	}else{
+		return "Now you ballin' in the Shinkansen!";
+	}
+	}
+	console.log(moneyTrain(121));
 /*
  * #14
  * Function - buyDoughnut
