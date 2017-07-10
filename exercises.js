fc7b5cc3312f1449a49f22e17c4923c62cb54c94
Justin Lau <jlau7248@gmail.com>
2017-07-10 12:25:52 -1000

    added myfavfoodsloop.

--- exercises.js
+++ exercises.js
@@ -367,6 +367,10 @@ for(var i = 1; i<=5; i++){
 */
   var myFavFoods = ["lemon bar", "carrot cake", "nachos", "bacon cheeseburger", "ramen", "sweet potato fries", "chimichanga"];
 
+for(var i =0; i<myFavFoods.length; i ++)
+{
+	console.log(myFavFoods[i]);
+}
 
 /*
  * #17
