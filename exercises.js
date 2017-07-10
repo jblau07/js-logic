ca1d367850531831bd78c51b01a0d891fa1a3385
Justin Lau <jlau7248@gmail.com>
2017-07-10 12:57:58 -1000

    added subways function

--- exercises.js
+++ exercises.js
@@ -446,6 +446,20 @@ console.log(west);
 
   var subOftheDay = ["Teriyaki Chicken", "Spicy Italian", "Turkey", "BMT", "Black Forest Ham", "Meatball Marinara", "Veggie"];
 
+function subways(special){
+	for(var i =0; i<special.length; i++){
+		if(i%2 === 1){
+		console.log(i);
+		special.splice(i, "Classic Tuna");
+		special[i]= "Classic Tuna"
+
+	}
+}
+return special;
+}
+
+subways(subOftheDay);
+console.log(subOftheDay);
 
 /*
 Final Boss
